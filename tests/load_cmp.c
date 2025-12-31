6138c8d18e99bc6962f0d36e5339f9e9f24b6255
ajs <sam4lordjesus@outlook.com>
2025-12-31 12:21:10 +0000

    added formatting settings

--- tests/load_cmp.c
+++ tests/load_cmp.c
@@ -1,152 +1,147 @@
 /*
-  Testing bignum_from_string and bignum_from_int 
+  Testing bignum_from_string and bignum_from_int
 
   Also testing bignum_cmp
 
 */
 
-
-#include <stdio.h>
-#include <string.h>
 #include <assert.h>
 #include <bn.h>
-
-
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-  char sabuf[8192];
-  char sbbuf[8192];
-  char scbuf[8192];
-  char sdbuf[8192];
-  char iabuf[8192];
-  char ibbuf[8192];
-  char icbuf[8192];
-  char idbuf[8192];
+	char sabuf[8192];
+	char sbbuf[8192];
+	char scbuf[8192];
+	char sdbuf[8192];
+	char iabuf[8192];
+	char ibbuf[8192];
+	char icbuf[8192];
+	char idbuf[8192];
 
-  struct bn sa, sb, sc, sd, se;
-  struct bn ia, ib, ic, id;
+	struct bn sa, sb, sc, sd, se;
+	struct bn ia, ib, ic, id;
 
-  bignum_init(&sa);
-  bignum_init(&sb);
-  bignum_init(&sc);
-  bignum_init(&sd);
-  bignum_init(&ia);
-  bignum_init(&ib);
-  bignum_init(&ic);
-  bignum_init(&id);
+	bignum_init(&sa);
+	bignum_init(&sb);
+	bignum_init(&sc);
+	bignum_init(&sd);
+	bignum_init(&ia);
+	bignum_init(&ib);
+	bignum_init(&ic);
+	bignum_init(&id);
 
-  printf("\nLoading numbers from strings and from int.\n");
-  
-  bignum_from_string(&sa, "000000FF", 8);
-  bignum_from_string(&sb, "0000FF00", 8);
-  bignum_from_string(&sc, "00FF0000", 8);
-  bignum_from_string(&sd, "FF000000", 8);
- 
-  bignum_from_int(&ia, 0x000000FF);
-  bignum_from_int(&ib, 0x0000FF00);
-  bignum_from_int(&ic, 0x00FF0000);
-  bignum_from_int(&id, 0xFF000000);
+	printf("\nLoading numbers from strings and from int.\n");
 
-  printf("Verifying comparison function.\n");
+	bignum_from_string(&sa, "000000FF", 8);
+	bignum_from_string(&sb, "0000FF00", 8);
+	bignum_from_string(&sc, "00FF0000", 8);
+	bignum_from_string(&sd, "FF000000", 8);
 
-  assert(bignum_cmp(&ia, &ib) == SMALLER);
-  assert(bignum_cmp(&ia, &ic) == SMALLER);
-  assert(bignum_cmp(&ia, &id) == SMALLER);
+	bignum_from_int(&ia, 0x000000FF);
+	bignum_from_int(&ib, 0x0000FF00);
+	bignum_from_int(&ic, 0x00FF0000);
+	bignum_from_int(&id, 0xFF000000);
 
-  assert(bignum_cmp(&ib, &ia) == LARGER);
-  assert(bignum_cmp(&ic, &ia) == LARGER);
-  assert(bignum_cmp(&id, &ia) == LARGER);
+	printf("Verifying comparison function.\n");
 
-  assert(bignum_cmp(&sa, &sb) == SMALLER);
-  assert(bignum_cmp(&sa, &sc) == SMALLER);
-  assert(bignum_cmp(&sa, &sd) == SMALLER);
+	assert(bignum_cmp(&ia, &ib) == SMALLER);
+	assert(bignum_cmp(&ia, &ic) == SMALLER);
+	assert(bignum_cmp(&ia, &id) == SMALLER);
 
-  assert(bignum_cmp(&sb, &sa) == LARGER);
-  assert(bignum_cmp(&sc, &sa) == LARGER);
-  assert(bignum_cmp(&sd, &sa) == LARGER);
+	assert(bignum_cmp(&ib, &ia) == LARGER);
+	assert(bignum_cmp(&ic, &ia) == LARGER);
+	assert(bignum_cmp(&id, &ia) == LARGER);
 
-  assert(bignum_cmp(&ia, &sa) == EQUAL);
-  assert(bignum_cmp(&ib, &sb) == EQUAL);
-  assert(bignum_cmp(&ic, &sc) == EQUAL);
-  assert(bignum_cmp(&id, &sd) == EQUAL);
+	assert(bignum_cmp(&sa, &sb) == SMALLER);
+	assert(bignum_cmp(&sa, &sc) == SMALLER);
+	assert(bignum_cmp(&sa, &sd) == SMALLER);
 
-  printf("Verifying to_string function.\n");
+	assert(bignum_cmp(&sb, &sa) == LARGER);
+	assert(bignum_cmp(&sc, &sa) == LARGER);
+	assert(bignum_cmp(&sd, &sa) == LARGER);
 
-  bignum_to_string(&sa, sabuf, sizeof(sabuf));
-  bignum_to_string(&sb, sbbuf, sizeof(sbbuf));
-  bignum_to_string(&sc, scbuf, sizeof(scbuf));
-  bignum_to_string(&sd, sdbuf, sizeof(sdbuf));
+	assert(bignum_cmp(&ia, &sa) == EQUAL);
+	assert(bignum_cmp(&ib, &sb) == EQUAL);
+	assert(bignum_cmp(&ic, &sc) == EQUAL);
+	assert(bignum_cmp(&id, &sd) == EQUAL);
 
-  bignum_to_string(&ia, iabuf, sizeof(iabuf));
-  bignum_to_string(&ib, ibbuf, sizeof(ibbuf));
-  bignum_to_string(&ic, icbuf, sizeof(icbuf));
-  bignum_to_string(&id, idbuf, sizeof(idbuf));
+	printf("Verifying to_string function.\n");
 
-  assert(strcmp(sabuf, iabuf) == 0);
-  assert(strcmp(sbbuf, ibbuf) == 0);
-  assert(strcmp(scbuf, icbuf) == 0);
-  assert(strcmp(sdbuf, idbuf) == 0);
+	bignum_to_string(&sa, sabuf, sizeof(sabuf));
+	bignum_to_string(&sb, sbbuf, sizeof(sbbuf));
+	bignum_to_string(&sc, scbuf, sizeof(scbuf));
+	bignum_to_string(&sd, sdbuf, sizeof(sdbuf));
 
-  printf("Verifying increment + decrement functions.\n");
+	bignum_to_string(&ia, iabuf, sizeof(iabuf));
+	bignum_to_string(&ib, ibbuf, sizeof(ibbuf));
+	bignum_to_string(&ic, icbuf, sizeof(icbuf));
+	bignum_to_string(&id, idbuf, sizeof(idbuf));
 
-  bignum_init(&sd);
-  int i;
-  for (i = 0; i < 255; ++i)
-  {
-    bignum_inc(&sd);
-    assert(!bignum_is_zero(&sd));
-  }
+	assert(strcmp(sabuf, iabuf) == 0);
+	assert(strcmp(sbbuf, ibbuf) == 0);
+	assert(strcmp(scbuf, icbuf) == 0);
+	assert(strcmp(sdbuf, idbuf) == 0);
 
-  assert(bignum_cmp(&sd, &ia) == EQUAL);
+	printf("Verifying increment + decrement functions.\n");
 
-  for (i = 0; i < 255; ++i)
-  {
-    assert(!bignum_is_zero(&sd));
-    bignum_dec(&sd);
-  }
-  assert(bignum_is_zero(&sd));
+	bignum_init(&sd);
+	int i;
+	for (i = 0; i < 255; ++i)
+	{
+		bignum_inc(&sd);
+		assert(!bignum_is_zero(&sd));
+	}
 
-  bignum_init(&sa);
-  bignum_init(&sb);
-  bignum_init(&sc);
-  bignum_init(&sd);
+	assert(bignum_cmp(&sd, &ia) == EQUAL);
 
-  char hex_1000[]    = "000003E8";
-  char hex_1000000[] = "000F4240";
+	for (i = 0; i < 255; ++i)
+	{
+		assert(!bignum_is_zero(&sd));
+		bignum_dec(&sd);
+	}
+	assert(bignum_is_zero(&sd));
 
-  /* Load 0x0308 into A and B from string */
-  bignum_from_string(&sa, hex_1000, 8);
-  bignum_from_string(&sb, hex_1000, 8);
+	bignum_init(&sa);
+	bignum_init(&sb);
+	bignum_init(&sc);
+	bignum_init(&sd);
 
-  /* Load 0x0308 into C from integer */
-  bignum_from_int(&sc, 0x3e8);
+	char hex_1000[] = "000003E8";
+	char hex_1000000[] = "000F4240";
 
-  /* Check for agreement - does bignum_from_string match bignum_from_int? */
-  assert(bignum_cmp(&sa, &sc) == EQUAL);
-  assert(bignum_cmp(&sb, &sc) == EQUAL);
+	/* Load 0x0308 into A and B from string */
+	bignum_from_string(&sa, hex_1000, 8);
+	bignum_from_string(&sb, hex_1000, 8);
 
-  /* Load comparison value: */
-  bignum_from_string(&sd, hex_1000000, 8);
-  bignum_from_int(&se, 0xf4240);
+	/* Load 0x0308 into C from integer */
+	bignum_from_int(&sc, 0x3e8);
 
-  /* Perform calculation:  C = A * B => C = 0x308 * 0x308 */
-  bignum_mul(&sa, &sb, &sc);
+	/* Check for agreement - does bignum_from_string match bignum_from_int? */
+	assert(bignum_cmp(&sa, &sc) == EQUAL);
+	assert(bignum_cmp(&sb, &sc) == EQUAL);
 
-  /* Check if results agree: */
-  assert(bignum_cmp(&sc, &sd) == EQUAL);
-  assert(bignum_cmp(&sc, &se) == EQUAL);
+	/* Load comparison value: */
+	bignum_from_string(&sd, hex_1000000, 8);
+	bignum_from_int(&se, 0xf4240);
 
-  /* Perform calculation other way around: C = B * A */
-  bignum_mul(&sb, &sa, &sc);
+	/* Perform calculation:  C = A * B => C = 0x308 * 0x308 */
+	bignum_mul(&sa, &sb, &sc);
 
-  /* Check if results agree: */
-  assert(bignum_cmp(&sc, &sd) == EQUAL);
-  assert(bignum_cmp(&sc, &se) == EQUAL);
+	/* Check if results agree: */
+	assert(bignum_cmp(&sc, &sd) == EQUAL);
+	assert(bignum_cmp(&sc, &se) == EQUAL);
 
+	/* Perform calculation other way around: C = B * A */
+	bignum_mul(&sb, &sa, &sc);
 
-  printf("\nTests successful.\n\n");
+	/* Check if results agree: */
+	assert(bignum_cmp(&sc, &sd) == EQUAL);
+	assert(bignum_cmp(&sc, &se) == EQUAL);
 
-  return 0;
+	printf("\nTests successful.\n\n");
+
+	return 0;
 }
-
--- tests/test_div_algo.c
+++ tests/test_div_algo.c
@@ -1,6 +1,6 @@
+#include <bn.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <bn.h>
 
 /*
 
@@ -10,105 +10,107 @@ To showcase different methods for implementing division
 
 */
 
-
 static int divide(int dividend, int divisor)
 {
-  int current = 1;
-  int denom = divisor;
-  // This step is required to find the biggest current number which can be
-  // divided with the number safely.
-  while (denom <= dividend) {
-    current <<= 1;
-    denom <<= 1;
-  }
-  // Since we may have increased the denomitor more than dividend
-  // thus we need to go back one shift, and same would apply for current.
-  denom >>= 1;
-  current >>= 1;
-  int answer = 0;
-  // Now deal with the smaller number.
-  while (current != 0)
-  {
-    if (dividend >= denom)
-    {
-      dividend -= denom;
-      answer |= current;
-    }
-    current >>= 1;
-    denom >>= 1;
-  }
-  return answer;
+	int current = 1;
+	int denom = divisor;
+	// This step is required to find the biggest current number which can be
+	// divided with the number safely.
+	while (denom <= dividend)
+	{
+		current <<= 1;
+		denom <<= 1;
+	}
+	// Since we may have increased the denomitor more than dividend
+	// thus we need to go back one shift, and same would apply for current.
+	denom >>= 1;
+	current >>= 1;
+	int answer = 0;
+	// Now deal with the smaller number.
+	while (current != 0)
+	{
+		if (dividend >= denom)
+		{
+			dividend -= denom;
+			answer |= current;
+		}
+		current >>= 1;
+		denom >>= 1;
+	}
+	return answer;
 }
 
-void divide2(unsigned dividend, unsigned divisor, unsigned* quotient, unsigned* remainder)
+void divide2(unsigned dividend, unsigned divisor, unsigned *quotient, unsigned *remainder)
 {
-  unsigned t, num_bits;
-  unsigned q, bit, d;
-  unsigned i;
+	unsigned t, num_bits;
+	unsigned q, bit, d;
+	unsigned i;
 
-  *remainder = 0;
-  *quotient = 0;
+	*remainder = 0;
+	*quotient = 0;
 
-  if (divisor == 0)
-    return;
+	if (divisor == 0)
+		return;
 
-  if (divisor > dividend) {
-    *remainder = dividend;
-    return;
-  }
+	if (divisor > dividend)
+	{
+		*remainder = dividend;
+		return;
+	}
 
-  if (divisor == dividend) {
-    *quotient = 1;
-    return;
-  }
+	if (divisor == dividend)
+	{
+		*quotient = 1;
+		return;
+	}
 
-  num_bits = 32;
+	num_bits = 32;
 
-  while ((*remainder) < divisor) {
-    bit = (dividend & 0x80000000) >> 31;
-    *remainder = ((*remainder) << 1) | bit;
-    d = dividend;
-    dividend = dividend << 1;
-    num_bits--;
-  }
+	while ((*remainder) < divisor)
+	{
+		bit = (dividend & 0x80000000) >> 31;
+		*remainder = ((*remainder) << 1) | bit;
+		d = dividend;
+		dividend = dividend << 1;
+		num_bits--;
+	}
 
-  /* The loop, above, always goes one iteration too far.
-     To avoid inserting an "if" statement inside the loop
-     the last iteration is simply reversed. */
-  dividend = d;
-  *remainder = (*remainder) >> 1;
-  num_bits++;
+	/* The loop, above, always goes one iteration too far.
+	   To avoid inserting an "if" statement inside the loop
+	   the last iteration is simply reversed. */
+	dividend = d;
+	*remainder = (*remainder) >> 1;
+	num_bits++;
 
-  for (i = 0; i < num_bits; i++) {
-    bit = (dividend & 0x80000000) >> 31;
-    *remainder = ((*remainder) << 1) | bit;
-    t = (*remainder) - divisor;
-    q = !((t & 0x80000000) >> 31);
-    dividend = dividend << 1;
-    *quotient = ((*quotient) << 1) | q;
-    if (q) {
-       *remainder = t;
-     }
-  }
-}  /* unsigned_divide */
+	for (i = 0; i < num_bits; i++)
+	{
+		bit = (dividend & 0x80000000) >> 31;
+		*remainder = ((*remainder) << 1) | bit;
+		t = (*remainder) - divisor;
+		q = !((t & 0x80000000) >> 31);
+		dividend = dividend << 1;
+		*quotient = ((*quotient) << 1) | q;
+		if (q)
+		{
+			*remainder = t;
+		}
+	}
+} /* unsigned_divide */
 
 int main(void)
 {
-  unsigned a,b,q,r;
+	unsigned a, b, q, r;
 
-  a = 255;
-  b = 8;
+	a = 255;
+	b = 8;
 
-  /*
-    a div b = q [with remainder r]
-  */
+	/*
+	  a div b = q [with remainder r]
+	*/
 
-  printf("1: %d / %d = %d \n", a,b,divide(a, b));
-  divide2(a,b,&q, &r);
-  printf("2: %d / %d = %d (%d) \n", a,b,q,r);
+	printf("1: %d / %d = %d \n", a, b, divide(a, b));
+	divide2(a, b, &q, &r);
+	printf("2: %d / %d = %d (%d) \n", a, b, q, r);
 
-
-  return 0;
+	return 0;
 }
-
-
